5bdce556c57a219bce8d8cada456d3bfc271b179
Ionut Cora <cora.ionut92@yahoo.com>
2025-01-24 09:42:08 +0000

    navigation bar style changes

--- src/Components/NavigationBar/NavigationBar.jsx
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './NavigationBar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMountainSun } from '@fortawesome/free-solid-svg-icons';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
-import AirplaneLogo from '../../Assets/Icons/airplane.svg';
 
 function NavigationBar() {
     return (
@@ -13,20 +14,20 @@ function NavigationBar() {
             <Container>
                 <Navbar.Brand className='text-light'>
                     <Link to='/' className='text-decoration-none text-light d-flex'>
-                        <img className='me-2' src={AirplaneLogo} alt="logo" />
-                        React Travel
+                        <FontAwesomeIcon icon={faMountainSun} size='xl' className='me-3' />
+                        Travel HUB
                     </Link>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls='basic-navbar-nav' className='text-light' />
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='me-auto w-100 justify-content-center'>
-                        <Link to='/' className='nav-link text-light text-uppercase'>Home</Link>
-                        <Link to='/holidays' className='nav-link text-light text-uppercase'>Holidays</Link>
-                        <Link to='/city-breaks' className='nav-link text-light text-uppercase'>City breaks</Link>
-                        <Link to='/destinations' className='nav-link text-light text-uppercase'>Destinations</Link>
+                        <Link to='/' className='nav-link text-light fw-semibold'>Home</Link>
+                        <Link to='/holidays' className='nav-link text-light fw-semibold'>Holidays</Link>
+                        <Link to='/city-breaks' className='nav-link text-light text-capitalize fw-semibold'>City breaks</Link>
+                        <Link to='/destinations' className='nav-link text-light fw-semibold'>Destinations</Link>
                     </Nav>
-                    <Button variant='light'>
-                        <Link to='/' className='text-decoration-none text-uppercase text-dark text-nowrap'>Sign in</Link>
+                    <Button variant='light' className='my-2 my-lg-0'>
+                        <Link to='/' className='text-decoration-none text-dark text-capitalize text-nowrap fw-semibold'>Sign in</Link>
                     </Button>
                 </Navbar.Collapse>
             </Container>
