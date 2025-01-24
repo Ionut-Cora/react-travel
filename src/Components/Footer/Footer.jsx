47a6ce4f8e6838f8eb180817ca8625817042d369
Ionut Cora <cora.ionut92@yahoo.com>
2025-01-24 10:02:56 +0000

    footer updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import Copyright from '../../Assets/Icons/c-circle.svg';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer() {
+  const [newDate, setNewDate] = useState('');
+  useEffect(() => {
+    let todayDate = new Date();
+    let todayYear = todayDate.getFullYear();
+    setNewDate([todayYear])
+  }, []);
+
   return (
     <div className='py-3'>
-        <p className='mb-0'>
-            Copyright
-            <img src={Copyright} className='mx-1' alt="copyright" />
-            Ionut Cora 2024
-        </p>
+        <p className='mb-0'>copyright &copy; made by <Link to='https://www.ionutcora.com/' style={{textDecoration: 'none', color: '#000000'}} target="_blank">Ionut Cora</Link> / 2024-{newDate}</p>
     </div>
   )
 }
